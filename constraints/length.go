8fbc2863eeb197cd482b8786d0497fee89ea0537
Hamid Asgari <hamidasgari313@gmail.com>
2025-11-24 15:14:25 +0330

    feat: put all length constraints inside length file

--- constraints/length.go
+++ constraints/length.go
@@ -12,6 +12,16 @@ var lengthError = errors.BasicError{
 	Message: "value length must be equal to {{length}}",
 }
 
+var minLengthError = errors.BasicError{
+	Code:    core.RuleMinLength,
+	Message: "value length must be at least {{minLength}}",
+}
+
+var maxLengthError = errors.BasicError{
+	Code:    core.RuleMaxLength,
+	Message: "value length must be at most {{maxLength}}",
+}
+
 // LengthString applies length constraint to a string.
 func LengthString(expectedLength int) core.StringValidator {
 	return func(value string) core.Error {
@@ -20,6 +30,34 @@ func LengthString(expectedLength int) core.StringValidator {
 				BasicError: lengthError,
 				ParamKey:   errors.ParamKeyLength,
 				ParamValue: int64(expectedLength),
+			}
+		}
+		return nil
+	}
+}
+
+// MinLengthString applies min length constraint to a string.
+func MinLengthString(minLength int) core.StringValidator {
+	return func(value string) core.Error {
+		if length := utf8.RuneCountInString(value); length < minLength {
+			return &errors.IntParamError{
+				BasicError: minLengthError,
+				ParamKey:   errors.ParamKeyMinLength,
+				ParamValue: int64(minLength),
+			}
+		}
+		return nil
+	}
+}
+
+// MaxLengthString applies max length constraint to a string.
+func MaxLengthString(maxLength int) core.StringValidator {
+	return func(value string) core.Error {
+		if length := utf8.RuneCountInString(value); length > maxLength {
+			return &errors.IntParamError{
+				BasicError: maxLengthError,
+				ParamKey:   errors.ParamKeyMaxLength,
+				ParamValue: int64(maxLength),
 			}
 		}
 		return nil
